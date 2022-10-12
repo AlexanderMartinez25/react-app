911092d826247908e6be78b9e27f3b85ff7f51be
Alexander <alexander.martinez@lapatota.cl>
2022-10-12 17:18:49 -0300

    Componente básico tradicional

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,12 @@
+import { ProductCard } from "../components/ProductCard"
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>Shoping Store</h1>
+      <hr />
+      <ProductCard />
+
+    </div>
+  )
+}
