8e98e95c727decd3ce6ffdfc225a6a7e84be9b37
Alexander <alexander.martinez@lapatota.cl>
2022-10-13 22:07:23 -0300

    Custom className

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,6 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
+import '../styles/custom-styles.css'
+
 
 const product = {
   id: '1',
@@ -24,7 +26,10 @@ export const ShoppingPage = () => {
           <ProductCard.Buttons />
         </ProductCard>
 
-        <ProductCard product={product}>
+        <ProductCard
+          product={product}
+          className="bg-dark"
+        >
           <ProductImage />
           <ProductTitle />
           <ProductButtons />
