2bde703e4aa419abe6a67a60f3f976fda8695a75
Alexander Martinez <alexander.martinez@lapatota.cl>
2022-10-18 20:33:09 -0300

    React CSSProperties

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -7,9 +7,10 @@ import styles from '../styles/styles.module.css'
 export interface Props {
   className?: string
   img?: string,
+  style?: React.CSSProperties;
 }
 
-export const ProductImage = ({ img, className }: Props) => {
+export const ProductImage = ({ img, className, style }: Props) => {
 
   const { product } = useContext(ProductContext);
   let imgToShow: string
@@ -26,6 +27,7 @@ export const ProductImage = ({ img, className }: Props) => {
     <img
       className={`${styles.productImg} ${className}`}
       src={imgToShow}
+      style={style}
       alt="Product Image"
     />
   );
