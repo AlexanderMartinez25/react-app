2c227057f9c423b20bde9a8ce80a759e8e977764
Alexander Martinez <alexander.martinez@lapatota.cl>
2022-10-26 22:02:43 -0300

    limpiando rama

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,11 @@
-import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom"
-import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage"
-import logo from '../assets/react.svg'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+} from "react-router-dom";
+import logo from "../assets/react.svg";
 
 export const Navigation = () => {
   return (
@@ -10,13 +15,28 @@ export const Navigation = () => {
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-active' : ''}>Shopping</NavLink>
+              <NavLink
+                to="/home"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                Shopping
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+              <NavLink
+                to="/users"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                Users
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -24,11 +44,10 @@ export const Navigation = () => {
         <Routes>
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
-          <Route path="home" element={<ShoppingPage />} />
-          <Route path="/*" element={<Navigate to='/home' replace />} />
+          <Route path="home" element={<h1>home</h1>} />
+          <Route path="/*" element={<Navigate to="/home" replace />} />
         </Routes>
-
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
