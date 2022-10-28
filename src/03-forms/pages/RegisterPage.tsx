096b641f20f73321f7b29f4e21715c04a7025dd6
Alexander Martinez <alexander.martinez@lapatota.cl>
2022-10-27 21:03:38 -0300

    Expandir funcionalidad de nuestro custom hook

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -4,7 +4,16 @@ import { useForm } from "../hooks/useForm";
 import "../styles/styles.css";
 
 export const RegisterPage = () => {
-  const { onChange, formData, email, name, password1, password2 } = useForm({
+  const {
+    onChange,
+    resetForm,
+    isValidEmail,
+    formData,
+    email,
+    name,
+    password1,
+    password2,
+  } = useForm({
     name: "",
     email: "",
     password1: "",
@@ -27,14 +36,18 @@ export const RegisterPage = () => {
           value={name}
           name="name"
           onChange={(e) => onChange(e)}
+          className={`${name.trim().length <= 0 && "has-error"}`}
         />
+        {name.trim().length <= 0 && <span>Este campo es necesario</span>}
         <input
           type="email"
           placeholder="Email"
           value={email}
           name="email"
           onChange={(e) => onChange(e)}
+          className={`${!isValidEmail(email) && "has-error"}`}
         />
+        {!isValidEmail(email) && <span>Este campo es necesario</span>}
         <input
           type="password"
           placeholder="Password"
@@ -42,6 +55,11 @@ export const RegisterPage = () => {
           name="password1"
           onChange={(e) => onChange(e)}
         />
+        {password1.trim().length <= 0 && <span>Este campo es necesario</span>}
+        {password2.trim().length > 0 && password1.trim().length > 0 && (
+          <span>La contraseña tiene que tener 6 caracteres</span>
+        )}
+
         <input
           type="password"
           placeholder="Repeat Password"
@@ -49,8 +67,15 @@ export const RegisterPage = () => {
           name="password2"
           onChange={(e) => onChange(e)}
         />
+        {password2.trim().length <= 0 && <span>Este campo es necesario</span>}
+        {password2.trim().length > 0 && password1 !== password2 && (
+          <span>las contraseñas deben de ser iguales</span>
+        )}
 
         <button type="submit">Create</button>
+        <button onClick={resetForm} type="button">
+          Reset Form
+        </button>
       </form>
     </div>
   );
